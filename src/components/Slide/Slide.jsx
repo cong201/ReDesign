3aeec5065c4ddeb216d9ddf971b3a7d0c0228897
Công Lê <levancong_t64@gmail.com>
2023-08-09 01:17:20 +0700

    fit again slide

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./slide.scss"
 import Slider from 'infinite-react-carousel';
-import { cards } from "../../data"
-import CatCard from "../CatCard/CatCard";
 
-const Slide = () => {
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
     return (
         <div className="slide">
             <div className="container">
-                <Slider slidesToShow={5} arrowsScroll={5}>
-                    {cards.map(card => (
-                        <CatCard item={card} key={card.id} />
-                    ))}
+                <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+                    {children}
                 </Slider>
             </div>
         </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,19 @@ import Featured from "../../components/Featured/Featured"
 import Slide from "../../components/Slide/Slide"
 import TrustBy from "../../components/TrustBy/TrustBy"
 import "./home.scss"
+import { cards } from "../../data"
+import CatCard from "../../components/CatCard/CatCard"
 
 const Home = () => {
     return (
         <div>
             <Featured />
             <TrustBy />
-            <Slide />
+            <Slide slidesToShow={5} arrowsScroll={5}>
+                {cards.map((card) => (
+                    <CatCard key={card.id} card={card} />
+                ))}
+            </Slide>
         </div>
     )
 }
